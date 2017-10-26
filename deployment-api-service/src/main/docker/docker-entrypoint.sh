f18f8e727101117a47736fef4c1bcabe4e8af656
Micah Noland <micah.noland@here.com>
2017-10-26 13:15:41 -0400

    Fixing Datadog integration AutoConfiguration.

--- deployment-api-service/src/main/docker/docker-entrypoint.sh
+++ deployment-api-service/src/main/docker/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 ./ssh-setup.sh
 	
 if [ -z "$1" -o  "${1:0:1}" = '-' ]; then
-    exec su-exec ${USER} java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar ${APP_HOME}/${APP_JAR} --loader.path=${APP_HOME}/lib --spring.cloud.config.server.git.uri=${GIT_URI} "$@"
+    exec su-exec ${USER} java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom  -Dloader.path=${APP_HOME}/lib -jar ${APP_HOME}/${APP_JAR} --spring.cloud.config.server.git.uri=${GIT_URI} "$@"
 fi
 
 exec "$@"
